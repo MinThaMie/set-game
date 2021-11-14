7037077fd2bbcc076779ae3010f5088863e281ca
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2021-11-14 15:06:22 +0100

    Restructure playing field functions + make things beautiful

--- app/components/playingfield.js
+++ app/components/playingfield.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { timeout , task} from 'ember-concurrency';
+import { timeout, task } from 'ember-concurrency';
 
 class Card {
   shape;
@@ -41,71 +41,9 @@ const amount = {
   3: 3,
 };
 
-function getDeck() {
-  let deck = []
-  for (let s in shapes) {
-    for (let a in amount) {
-      for (let f in fillings) {
-        for (let c in colors) {
-          let card = new Card({ s, a, f, c });
-          deck.push(card);
-        }
-      }
-    }
-  }
-  return deck;
-}
-
-function isSet(a, b, c) {
-  return (
-    validateProps(a.shape, b.shape, c.shape) &&
-    validateProps(a.amount, b.amount, c.amount) &&
-    validateProps(a.filling, b.filling, c.filling) &&
-    validateProps(a.color, b.color, c.color)
-  );
-}
-
-function validateProps(a, b, c) {
-  return (a === b && b === c) || (a !== b && a !== c && b !== c);
-}
-
-function k_combinations(set, k) {
-  var i, j, combs, head, tailcombs;
-  if (k > set.length || k <= 0) {
-    return [];
-  }
-  if (k == set.length) {
-    return [set];
-  }
-  if (k == 1) {
-    combs = [];
-    for (i = 0; i < set.length; i++) {
-      combs.push([set[i]]);
-    }
-    return combs;
-  }
-  combs = [];
-  for (i = 0; i < set.length - k + 1; i++) {
-    head = set.slice(i, i + 1);
-    tailcombs = k_combinations(set.slice(i + 1), k - 1);
-    for (j = 0; j < tailcombs.length; j++) {
-      combs.push(head.concat(tailcombs[j]));
-    }
-  }
-  return combs;
-}
-
-function getRandomCard(cards) {
-  const randomIndex = Math.floor(Math.random() * cards.length);
-  let randomCard = cards[randomIndex];
-  cards.splice(randomIndex, 1);
-  return {rc: randomCard, cards: cards};
-}
-
 export default class PlayingfieldComponent extends Component {
   @tracked count = 0;
   @tracked isStarted = false;
-  @tracked isWon = false;
   @tracked field = [];
   @tracked selected = [];
   @tracked cards = [];
@@ -113,8 +51,8 @@ export default class PlayingfieldComponent extends Component {
   @tracked finishTime = 0;
 
   get hasSet() {
-    const combinations = k_combinations(this.field, 3);
-    let foundSet = combinations.find((comb) => isSet(...comb));
+    const combinations = this.k_combinations(this.field, 3);
+    let foundSet = combinations.find((comb) => this.isSet(...comb));
     if (foundSet) {
       return true;
     } else {
@@ -122,19 +60,112 @@ export default class PlayingfieldComponent extends Component {
     }
   }
 
-  @action startGame() {
-    this.isStarted = true;
-    this.isWon = false;
-    this.field = [];
-    this.cards = getDeck();
-    this.time = 0;
-    for (let i = 1; i <= 12; i++) {
-      let {rc, cards} = getRandomCard(this.cards)
+  get isWon() {
+    return this.isStarted && this.cards.length == 0 && !this.hasSet;
+  }
+
+  getDeck() {
+    let deck = [];
+    for (let s in shapes) {
+      for (let a in amount) {
+        for (let f in fillings) {
+          for (let c in colors) {
+            let card = new Card({ s, a, f, c });
+            deck.push(card);
+          }
+        }
+      }
+    }
+    return deck;
+  }
+
+  isSet(a, b, c) {
+    return (
+      this.validateProps(a.shape, b.shape, c.shape) &&
+      this.validateProps(a.amount, b.amount, c.amount) &&
+      this.validateProps(a.filling, b.filling, c.filling) &&
+      this.validateProps(a.color, b.color, c.color)
+    );
+  }
+
+  validateProps(a, b, c) {
+    return (a === b && b === c) || (a !== b && a !== c && b !== c);
+  }
+
+  k_combinations(set, k) {
+    var i, j, combs, head, tailcombs;
+    if (k > set.length || k <= 0) {
+      return [];
+    }
+    if (k == set.length) {
+      return [set];
+    }
+    if (k == 1) {
+      combs = [];
+      for (i = 0; i < set.length; i++) {
+        combs.push([set[i]]);
+      }
+      return combs;
+    }
+    combs = [];
+    for (i = 0; i < set.length - k + 1; i++) {
+      head = set.slice(i, i + 1);
+      tailcombs = this.k_combinations(set.slice(i + 1), k - 1);
+      for (j = 0; j < tailcombs.length; j++) {
+        combs.push(head.concat(tailcombs[j]));
+      }
+    }
+    return combs;
+  }
+
+  getRandomCard(cards) {
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    let randomCard = cards[randomIndex];
+    cards.splice(randomIndex, 1);
+    return { rc: randomCard, cards: cards };
+  }
+
+  getCards(amount) {
+    for (let i = 1; i <= amount; i++) {
+      let { rc, cards } = this.getRandomCard(this.cards);
       this.field.push(rc);
       this.cards = cards;
     }
-    this.field = [...this.field];
+  }
+
+  checkPotentialSet() {
+    const picked = this.field.filter((c) => c.selected);
+    this.selected = [];
+    if (this.isSet(...picked)) {
+      this.field = this.field.filter((c) => !c.selected);
+      if (this.cards.length > 0 && this.field.length < 12) {
+        this.getCards(3);
+      }
+      while (!this.hasSet && this.cards.length > 0) {
+        this.getCards(3);
+      }
+      if (this.isWon) {
+        this.finishTime = this.time;
+      }
+      this.count += 1;
+    } else {
+      picked.forEach((p) => {
+        p.wrong = true;
+        setTimeout(() => {
+          p.wrong = false;
+        }, 820);
+      });
+      this.field.forEach((card) => (card.selected = false));
+    }
+  }
+
+  @action startGame() {
+    this.cards = this.getDeck();
+    this.getCards(12);
+    this.time = 0;
+    this.count = 0;
     this.timerTask.perform();
+    this.isStarted = true;
   }
 
   @action selectCard(id) {
@@ -146,45 +177,13 @@ export default class PlayingfieldComponent extends Component {
       this.field.find((x) => x.image === id).selected = true;
     }
     if (this.selected.length === 3) {
-      const picked = this.field.filter((c) => c.selected);
-      this.selected = [];
-      const isValidSet = isSet(...picked);
-      if (isValidSet) {
-        this.field = this.field.filter((c) => !c.selected);
-        if (this.cards.length > 0 && this.field.length < 12) {
-          for (let i = 1; i <= 3; i++) {
-            let {rc, cards} = getRandomCard(this.cards)
-            this.field.push(rc);
-            this.cards = cards;
-          }
-        }
-        if (!this.hasSet && this.cards.length > 0) {
-          for (let i = 1; i <= 3; i++) {
-            let {rc, cards} = getRandomCard(this.cards)
-            this.field.push(rc);
-            this.cards = cards;
-          }
-        }
-        if (this.cards.length == 0 && !this.hasSet) {
-          this.isWon = true;
-          this.finishTime = this.time;
-        }
-        this.count += 1;
-      } else {
-        picked.forEach((p) => {
-          p.wrong = true;
-          setTimeout(() => {
-            p.wrong = false;
-          }, 820);
-        });
-        this.field.forEach((card) => (card.selected = false));
-      }
+      this.checkPotentialSet();
     }
     this.field = [...this.field];
   }
 
   @task *timerTask() {
-    while(true) {
+    while (true) {
       yield timeout(1000);
       this.time += 1;
     }
