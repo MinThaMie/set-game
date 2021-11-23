63d11631847464867274c9a5da28928f546b1e4a
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2021-11-23 09:56:23 +0100

    Adds manifest for PWA stuff

---
+++ config/manifest.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function (/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: 'set-game',
+    short_name: 'set-game',
+    description: '',
+    start_url: '/',
+    scope: '/',
+    display: 'standalone',
+    background_color: '#673ab7',
+    theme_color: '#673ab7',
+    icons: [
+      {
+        src: '/images/logo/logo.png',
+        sizes: '180x180',
+      },
+      {
+        src: '/images/logo/splash-screen.png',
+        sizes: '512x512',
+      },
+    ],
+    ms: {
+      tileColor: '#673ab7',
+    },
+  };
+};
