b84705b9d27a835316a5c25fcf7b0fc4af13103b
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2022-10-03 16:47:03 +0200

    Update tests

--- tests/integration/components/card-test.js
+++ tests/integration/components/card-test.js
@@ -9,16 +9,17 @@ module('Integration | Component | card', function (hooks) {
   test('it renders', async function (assert) {
     // Render unselected card:
     await render(hbs`
-      <Card @image="images/diamond1solid_green.svg" @selected={{false}} @wrong={{false}} @hint={{false}}/>
+      <Card @id="diamond2solid_green" @shape="diamond" @amount="2" @filling="solid" @color="green" @selected={{false}} @wrong={{false}} @hint={{false}}/>
     `);
-    assert.dom('img').hasAttribute('src', 'images/diamond1solid_green.svg');
+    assert.dom('[data-test-card-diamond-solid]').exists({count: 2});
+
   });
 
   test('it replaces hint styling with selected styling', async function (assert) {
     // Render selected card that is hinted
     await render(hbs`
-     <Card @image="images/diamond1solid_green.svg" @selected={{true}} @wrong={{false}} @hint={{true}}/>
-   `);
+    <Card @id="diamond2solid_green" @shape="diamond" @amount="2" @filling="solid" @color="green" @selected={{true}} @wrong={{false}} @hint={{true}}/>
+  `);
     assert.dom('[data-test-card]').hasStyle({
       border: '3px solid rgb(255, 165, 0)',
       borderWidth: '3px',
